d0c2ee4cdf428648b9f8e64075f071f7fcc4fdc7
Sanat Sharma <sanatcolumban@gmail.com>
2020-06-08 15:19:10 -0500

    institutes code

--- examples/demo/src/institutes/index.js
+++ examples/demo/src/institutes/index.js
@@ -1,7 +1,7 @@
 import InstituteIcon from '@material-ui/icons/School';
 //import ProductList from './ProductList';
 //import ProductEdit from './ProductEdit';
-//import ProductCreate from './ProductCreate';
+import InstituteCreate from './InstituteCreate';
 
 export default {
     icon: InstituteIcon,

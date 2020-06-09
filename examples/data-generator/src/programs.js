a891d23627b711f3315889bcaa2a41afdf7c311e
Adarsh Venkataraman <adarsh9804@gmail.com>
2020-06-09 02:27:00 -0500

    Rebuilt data-generator files

--- examples/data-generator/src/programs.js
+++ examples/data-generator/src/programs.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var en_1 = require("faker/locale/en");
 var utils_1 = require("./utils"); 
 var cohort_id = 0;
-var array = [];
+var outcomes_list = [];
 function randomNumber(min, max) {
     return Math.random() * (max - min) + min;
 }
@@ -11,15 +11,15 @@ function randomNumber(min, max) {
 function generate_outcomes() {
     var i;
     for (i = 0; i < 10; i++) {
-        array.push(en_1.random.word);
+        outcomes_list.push(en_1.random.word());
     }
 }
 exports.default = (function (db, _a) {
     var serializeDate = _a.serializeDate;
     return Array.from(Array(900).keys()).map(function (id) {
-        var department_name = en_1.lorem.text();
+        var department_name = en_1.random.words();
         var code = Math.round(utils_1.randomFloat());
-        var accreditation_status = en_1.random.words();
+        var accreditation_status = en_1.random.word();
         var start_year = Math.round(randomNumber(2000, 2020));
         var intake = Math.round(utils_1.randomFloat());
         var outcomes= generate_outcomes();

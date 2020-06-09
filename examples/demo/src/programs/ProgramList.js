e44e12d9fcbb1b9c2bf0ca1eb207272917151fc2
Adarsh Venkataraman <adarsh9804@gmail.com>
2020-06-08 20:59:14 -0500

    Made changes based on comments

--- examples/demo/src/programs/ProgramList.js
+++ examples/demo/src/programs/ProgramList.js
@@ -1,15 +1,10 @@
 import * as React from 'react';
-import { Fragment } from 'react';
 import {
     AutocompleteInput,
-    BooleanField,
     Datagrid,
-    DateField,
     DateInput,
     Filter,
     List,
-    NullableBooleanInput,
-    NumberField,
     ReferenceInput,
     SearchInput,
     TextField,
@@ -18,23 +13,16 @@ import {
 import {
     makeStyles,
     useMediaQuery,
-    Divider,
-    Tabs,
-    Tab,
 } from '@material-ui/core';
-
-//import NbItemsField from './NbItemsField';
-//import CustomerReferenceField from '../visitors/CustomerReferenceField';
-//import MobileGrid from './MobileGrid'; 
 
 const ProgramFilter = props => (
     <Filter {...props}>
         <SearchInput source="q" alwaysOn />
-        <ReferenceInput source="cohort_id" reference="name">
+        <ReferenceInput source="cohort_id" reference="cohorts">
             <AutocompleteInput
                 optionText={choice =>
-                    choice.first_name && choice.last_name
-                        ? `${choice.first_name} ${choice.last_name}`
+                    choice.department_name
+                        ? `${choice.department_name}`
                         : ''
                 }
             />
@@ -47,15 +35,6 @@ const useDatagridStyles = makeStyles({
     total: { fontWeight: 'bold' },
 });
 
-// const ProgramList = (props) => (
-//     <List {...props} >
-//         <Datagrid>
-//             <TextField source= "cohort_id"/>
-//             <TextField source= "department_name"/>
-//         </Datagrid> 
-//     </List>
-// );
-
 const ProgramList = ({ classes, ...props }) => (
     <List
         {...props}
@@ -66,6 +45,11 @@ const ProgramList = ({ classes, ...props }) => (
         <Datagrid {...props}> 
             <TextField source= "cohort_id"/>
             <TextField source= "department_name"/>
+            <TextField source= "code"/>
+            <TextField source= "accreditation_status"/>
+            <TextField source= "name"/>
+            <TextField source= "start_year"/>
+            <TextField source= "outcomes"/>
         </Datagrid> 
     </List>
 );

f25d0c8e9cbff2e8e586818bbd5ab8f64261619f
Sanat Sharma <sanatcolumban@gmail.com>
2020-06-10 22:52:46 -0500

    merge complete

--- examples/data-generator/src/index.js
+++ examples/data-generator/src/index.js
@@ -4,14 +4,10 @@ import generateProducts from './products';
 import generateCommands from './commands';
 import generateInvoices from './invoices';
 import generateReviews from './reviews';
-<<<<<<< HEAD
 import generatePrograms from './programs';
-import finalize from './finalize'; 
-=======
 import finalize from './finalize';
 import generateInsitutes from './institutes';
 import generateCohorts from './cohorts';
->>>>>>> 7fc003b8c7b4ba6fbfec69d6ddc7bea1ede4c6b5
 
 export default (options = { serializeDate: true }) => {
     const db = {};
@@ -21,12 +17,9 @@ export default (options = { serializeDate: true }) => {
     db.commands = generateCommands(db, options);
     db.invoices = generateInvoices(db, options);
     db.reviews = generateReviews(db, options);
-<<<<<<< HEAD
     db.programs = generatePrograms(db, options);
-=======
     db.institutes = generateInsitutes(db, options);
     db.cohorts = generateCohorts(db, options);
->>>>>>> 7fc003b8c7b4ba6fbfec69d6ddc7bea1ede4c6b5
     finalize(db);
 
     return db;

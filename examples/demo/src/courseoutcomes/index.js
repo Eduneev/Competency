97fbb9aa1d0902ca72bd5fc92f990a3e8e5c1d83
Sanat Sharma <sanatcolumban@gmail.com>
2020-06-16 18:51:17 -0500

    course outcome files

--- examples/demo/src/courseoutcomes/index.js
+++ examples/demo/src/courseoutcomes/index.js
@@ -1,5 +1,7 @@
 import CourseOutcomeIcon from '@material-ui/icons/Note';
+import CourseOutcomeList from './CourseOutcomeList';
 
 export default {
     icon: CourseOutcomeIcon,
+    list: CourseOutcomeList,
 };

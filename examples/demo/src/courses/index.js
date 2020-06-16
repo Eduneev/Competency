97779b97d87c483b39c5c92c5100ec75d8b936b9
Sanat Sharma <sanatcolumban@gmail.com>
2020-06-16 18:52:00 -0500

    course stuff

--- examples/demo/src/courses/index.js
+++ examples/demo/src/courses/index.js
@@ -1,7 +1,9 @@
 import CourseIcon from '@material-ui/icons/Bookmarks';
 import CourseList from './CourseList';
+import CourseEdit from './CourseEdit';
 
 export default {
     icon: CourseIcon,
     list: CourseList,
+    edit: CourseEdit,
 };

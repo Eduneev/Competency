211273ed60a6cc06592472fdc3b6c96dcfd831fb
Adarsh Venkataraman <adarsh9804@gmail.com>
2020-06-23 23:20:19 -0500

    Added edit functionality for course outcomes

--- examples/demo/src/courseoutcomes/index.js
+++ examples/demo/src/courseoutcomes/index.js
@@ -1,9 +1,11 @@
 import CourseOutcomeIcon from '@material-ui/icons/Note';
 import CourseOutcomeList from './CourseOutcomeList';
 import CourseOutcomeCreate from './CourseOutcomeCreate';
+import CourseOutcomeEdit from './CourseOutcomeEdit';
 
 export default {
     icon: CourseOutcomeIcon,
-    list: CourseOutcomeList,
+    //list: CourseOutcomeList,
     create: CourseOutcomeCreate,
+    edit: CourseOutcomeEdit,
 };

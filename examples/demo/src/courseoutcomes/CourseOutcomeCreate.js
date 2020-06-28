ebe7012116cc84acec2fc00517bac6b56d75aac0
Sanat Sharma <sanatcolumban@gmail.com>
2020-06-28 03:14:16 -0500

    CO create

--- examples/demo/src/courseoutcomes/CourseOutcomeCreate.js
+++ examples/demo/src/courseoutcomes/CourseOutcomeCreate.js
@@ -7,6 +7,8 @@ import {
     Toolbar,
     translate,
     TextInput,
+    ReferenceInput,
+    AutocompleteInput,
 } from 'react-admin';
 import Button from '@material-ui/core/Button';
 
@@ -14,17 +16,32 @@ const CourseOutcomeCreateToolbar = translate(
     ({ onCancel, translate, ...props }) => (
         <Toolbar {...props}>
             <SaveButton />
-            <Button onClick={onCancel}>{translate('ra.action.cancel')}</Button>
+            <Button redirect={onCancel}>{translate('ra.action.cancel')}</Button>
         </Toolbar>
     )
 );
 
 const CourseOutcomeCreate = ({ onCancel, ...props }) => {
+    if (props.location.state === 'undefined') return null;
+
+    const course_id = props.location.state['course_id'];
+    const redirect = `/courses/${course_id}`;
     return (
         <Create {...props}>
             <SimpleForm
-                toolbar={<CourseOutcomeCreateToolbar onCancel={onCancel} />}
+                redirect={redirect}
+                toolbar={<CourseOutcomeCreateToolbar onCancel={redirect} />}
             >
+                <ReferenceInput
+                    source="course_id"
+                    reference="courses"
+                    validate={required()}
+                    defaultValue={course_id}
+                >
+                    <AutocompleteInput
+                        optionText={choice => `${choice.name}`}
+                    />
+                </ReferenceInput>
                 <TextInput source="description" validate={required()} />
             </SimpleForm>
         </Create>

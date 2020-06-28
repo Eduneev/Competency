ee7a559243ef18045c683597c849636edecf72c9
Bharath Kaimal <bharath.kaimal@gmail.com>
2020-06-28 16:11:06 -0500

    Program edit page created with associated parameters

--- examples/demo/src/programs/ProgramEdit.js
+++ examples/demo/src/programs/ProgramEdit.js
@@ -1,21 +1,31 @@
 import * as React from 'react';
 import {
+    AutocompleteInput,
+    BooleanInput,
+    DateInput,
     Edit,
+    ReferenceInput,
     SelectInput,
     SimpleForm,
     useTranslate,
+    required,
     TextInput,
+    NumberField,
+    TextField,
+    ColoredNumberField,
+    InstituteReferenceField,
     NumberInput,
 } from 'react-admin';
 import { makeStyles } from '@material-ui/core/styles';
-import ProgramOutcome from './ProgramOutcome';
+
+//import Basket from './Basket';
 
 const ProgramTitle = ({ record }) => {
     const translate = useTranslate();
     return (
         <span>
-            {translate('resources.programs.title', {
-                reference: record.name,
+            {translate('resources.commands.title', {
+                reference: record.reference,
             })}
         </span>
     );
@@ -28,38 +38,58 @@ const useEditStyles = makeStyles({
 const ProgramEdit = props => {
     const classes = useEditStyles();
     return (
-        <Edit 
-            title={<ProgramTitle />} 
-            classes={classes} 
-            aside={<ProgramOutcome />}
-            {...props}>
+        <Edit
+            title={<ProgramTitle />}
+            //aside={<Basket />}
+            classes={classes}
+            {...props}
+        >
             <SimpleForm>
                 <TextInput source="name" />
+                {//<InstituteReferenceField />
+}
                 <TextInput source="department_name" />
                 <NumberInput source="start_year" />
+                {//<ColoredNumberField source="intake" />
+}
                 <NumberInput source="intake" />
+                {//<TextField source="accreditation_status" />
+}
+                {/* <ReferenceInput source="institute" reference="customers">
+                    <AutocompleteInput
+                        optionText={choice =>
+                            `${choice.first_name} ${choice.last_name}`
+                        }
+                    />
+                </ReferenceInput>
+                <ReferenceInput
+                    source="department name"
+                    reference="customers"
+                    validate={required()}
+                >
+                    <AutocompleteInput
+                        optionText={choice => `${choice.email}`}
+                    />
+                </ReferenceInput> */}
+
                 <SelectInput
-                    source="accreditation_status"
+                    source="accreditation status"
                     choices={[
+                        { id: 'notaccredited', name: 'Not Accredited' },
+                        { id: 'accredited', name: 'Accredited' },
+                        { id: 'firsttime', name: 'Applying First Time' },
                         {
-                            id: 'Applying First Time',
-                            name: 'Applying First Time',
+                            id: 'unknown',
+                            name: 'unknown',
+                            disabled: true,
                         },
-                        {
-                            id: 'Provisional Status for 2-3 years',
-                            name: 'Provisional Status for 2-3 years',
-                        },
-                        {
-                            id: 'Granted accreditation for 5/6 years',
-                            name: 'Granted accreditation for 5/6 years',
-                        },
-                        { id: 'Not Accredited', name: 'Not Accredited' },
                     ]}
-                    optionText="name"
                 />
+                {//<BooleanInput source="returned" />
+}
             </SimpleForm>
         </Edit>
     );
-};
+}; 
 
 export default ProgramEdit;

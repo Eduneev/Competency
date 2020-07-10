27b122e6dc629bf307160419ee02999ec606042b
Sanat Sharma <sanatcolumban@gmail.com>
2020-07-09 23:48:45 -0500

    Add Cancel redirection for CO

--- examples/demo/src/courseoutcomes/CourseOutcomeCreate.js
+++ examples/demo/src/courseoutcomes/CourseOutcomeCreate.js
@@ -10,27 +10,30 @@ import {
     ReferenceInput,
     AutocompleteInput,
 } from 'react-admin';
-import Button from '@material-ui/core/Button';
+import CancelButton from './CancelButton';
 
-const CourseOutcomeCreateToolbar = translate(
-    ({ onCancel, translate, ...props }) => (
-        <Toolbar {...props}>
-            <SaveButton />
-            <Button redirect={onCancel}>{translate('ra.action.cancel')}</Button>
-        </Toolbar>
-    )
-);
+const CourseOutcomeCreateToolbar = translate(({ translate, ...props }) => (
+    <Toolbar {...props}>
+        <SaveButton />
+        <CancelButton
+            variant="outlined"
+            color="secondary"
+            style={{ marginLeft: '1rem' }}
+        />
+    </Toolbar>
+));
 
 const CourseOutcomeCreate = ({ onCancel, ...props }) => {
     if (props.location.state === 'undefined') return null;
 
     const course_id = props.location.state['course_id'];
+    console.log(course_id);
     const redirect = `/courses/${course_id}`;
     return (
         <Create {...props}>
             <SimpleForm
                 redirect={redirect}
-                toolbar={<CourseOutcomeCreateToolbar onCancel={redirect} />}
+                toolbar={<CourseOutcomeCreateToolbar />}
             >
                 <ReferenceInput
                     source="course_id"

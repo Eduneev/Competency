381edf906f3a4e1cd330ec38859b98ba3711e3ea
Bharath Kaimal <bharath.kaimal@gmail.com>
2020-07-29 18:47:51 -0500

    index.js update

--- examples/demo/src/cohorts/index.js
+++ examples/demo/src/cohorts/index.js
@@ -2,8 +2,12 @@ import CohortsIcon from '@material-ui/icons/Group';
 //import ProductList from './ProductList';
 import CohortList from './CohortList';
 //import ProductCreate from './ProductCreate';
+import CohortEdit from './CohortEdit';
+import CohortCreate from './CohortCreate';
 
 export default {
     icon: CohortsIcon,
     list: CohortList,
+    edit: CohortEdit,
+    create: CohortCreate,
 };
